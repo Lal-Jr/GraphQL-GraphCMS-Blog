281171f21b0e2a52091a84e6d6b5c6d3591e222e
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2021-12-14 21:40:13 +0530

    posts data fetching using graphql and integration

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,11 +9,11 @@ const categories = [
 const Header = () => {
 	return (
 		<div className="container mx-auto px-10 mb-8">
-			<div classname="border-b w-full inline-block border-blue-400 py-8">
+			<div className="border-b w-full inline-block border-blue-400 py-8">
 				<div className="md:float-left block">
 					<Link href="/">
 						<span className="cursor-pointer font-bold text-4xl text-white">
-							GraphCMS
+							Graph CMS
 						</span>
 					</Link>
 				</div>
